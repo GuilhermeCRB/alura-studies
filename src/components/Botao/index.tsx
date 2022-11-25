d8481b4b584edca9d8f0d756e4dcea1f7b0b5842
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-11-25 19:29:09 -0300

    feat: creates form

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,13 +1,9 @@
 import React from "react";
 
-class Botao extends React.Component {
-    render() {
-     return (
-        <button>
-          Botão
-        </button>
-      )
-   }
+export default function Botao() {
+  return (
+    <button>
+      Botão
+    </button>
+  )
 }
-
-export default Botao;
