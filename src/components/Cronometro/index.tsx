8c85b93d20b6a82465a4e86ccc69fdd66c704490
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-11-27 19:41:34 -0300

    feat: adds time to stopwatch

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,17 +1,33 @@
+import { useEffect, useState } from 'react';
+
 import style from './style.module.scss'
 import Botao from "../Botao";
 import Relogio from "./Relogio";
+import ITarefa from '../../types/tarefa';
+import { tempoParaSegundos } from '../../common/utils/tempo';
 
-export default function Cronometro() {
-    return (
-        <div className={style.cronometro}>
-           <p className={style.titulo}> Escolha um card e inicie o cronômetro </p>
-        <div className={style.relogioWrapper}>
-           <Relogio />
-           </div>
-           <Botao>
-              Começar!
-           </Botao>
+interface Props {
+    selecionado: ITarefa | undefined
+}
+
+export default function Cronometro({ selecionado }: Props) {
+   const [tempo, setTempo] = useState<number>();
+
+   useEffect(() => {
+      if(selecionado?.tempo) {
+        setTempo(tempoParaSegundos(selecionado.tempo));
+      }
+   },[selecionado]);
+
+   return (
+      <div className={style.cronometro}>
+         <p className={style.titulo}> Escolha um card e inicie o cronômetro </p>
+      <div className={style.relogioWrapper}>
+         <Relogio tempo={tempo} />
+         </div>
+         <Botao>
+            Começar!
+         </Botao>
       </div>
-    )
+   )
 }
